5a3497a798ed7c948ec91e95be4ed1a9c86931f9
Muktadeer <muktadeer@bdjobs.local>
2026-02-05 15:36:35 +0600

    ok

---
+++ apps/app-one/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { Layout } from 'libs/shared-ui/src/lib/layout/layout';
+
+export const appRoutes: Route[] = [
+  { path: '', component: Layout },
+];
