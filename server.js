b5de3b6972e5dfeaa07a9c21f28f123920a1df19
ma-muktadeer <muktadeer.ma@gmail.com>
2026-02-07 12:32:40 +0600

    socket ok

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
   });
   socket.on('message', (data) => {
     console.log('Message received: ', data);
-    socket.emit('message', { message: 'Message received ' + data });
+    io.emit('message', { message: data });
   });
   socket.on('disconnect', () => {
     console.log('User disconnected');
