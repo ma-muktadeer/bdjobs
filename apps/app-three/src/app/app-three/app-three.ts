9d07cda121c3e0e3a4e12e02a61994d51cb1bbef
ma-muktadeer <muktadeer.ma@gmail.com>
2026-03-03 12:00:01 +0600

    up

---
+++ apps/app-three/src/app/app-three/app-three.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-three',
+  imports: [],
+  templateUrl: './app-three.html',
+  styleUrl: './app-three.css',
+})
+export class AppThree {}
