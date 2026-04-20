26d6c0eb9e8181bee624da7723d5aaa8a6a903f7
ma-muktadeer <muktadeer.ma@gmail.com>
2026-04-20 16:45:07 +0600

    lzy

--- apps/app-one/src/app/app.routes.ts
+++ apps/app-one/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Route } from '@angular/router';
-import { Layout } from '@company-workspace/shared-ui';
 import { Sms } from './sms/sms';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: Layout,
+    loadComponent: () => import('@company-workspace/shared-ui').then(m => m.Layout),
     children: [
       { path: 'sms', component: Sms, },
       { path: '', redirectTo: 'sms', pathMatch: 'full' },
